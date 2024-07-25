346576d4fde3734cdd6ce0c26446a64614f1c266
metheglin <pigmybank@gmail.com>
2024-07-25 16:32:02 +0900

    Improved inspect.

--- lib/giri/base_node.rb
+++ lib/giri/base_node.rb
@@ -1,3 +1,7 @@
 class Giri::BaseNode
   extend Giri::Bud
+
+  def inspect
+    "#<#{self.class}:0x#{object_id.to_s(16)} @ref_name=#{@ref_name.inspect} parent_ref_name=#{parent_ref_name.inspect} @attributes=#{@attributes.inspect}>"
+  end
 end
--- lib/giri/bud.rb
+++ lib/giri/bud.rb
@@ -62,10 +62,6 @@ module Giri::Bud
           [name.to_sym, value]
         end.to_h
       end
-
-      def inspect
-        "#<#{self.class}:0x#{object_id.to_s(16)} @ref_name=#{@ref_name.inspect} parent_ref_name=#{parent_ref_name.inspect} @attributes=#{@attributes.inspect}>"
-      end
     end
   end
 
